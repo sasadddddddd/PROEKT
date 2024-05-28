90562fdb806523d7f44f0ab9af7205234a56bacc
sasadddddddd <sanagolubec5@gmail.com>
2024-05-28 23:01:40 +0300

    Update labs.h

--- labs.h
+++ labs.h
@@ -1,7 +1,4 @@
-#ifndef LABS_H_INCLUDED
-#define LABS_H_INCLUDED
-#define MAX_LENGTH 100
-#define N 5
+#include <stdio.h>
 int lab1();
 int lab2();
 int lab3();
@@ -9,5 +6,4 @@ int lab4();
 int lab5();
 int lab4a();
 int lab5a();
-#endif
 
